e0a766c3246a126998f3111640be235d4b4fe902
W84Riko <ymykhalyna@gmail.com>
2019-10-18 16:37:53 +0000

    Tasks made

--- js/product-list.js
+++ js/product-list.js
@@ -4,39 +4,46 @@ class ProductList {
     fetch(productsUrl)
       .then(result => result.json())
       .then(products => {
+        products.sort((a, b) => a.price - b.price);
         this.products = products;
         this.renderProducts(renderContainer, products);
         this.addEventListeners();
       });
+    document.querySelector('.filter').addEventListener('keydown',
+      this.renderProducts(renderContainer, products));
   }
   getProductById(id) {
     return this.products.find(el => el.id === id);
   }
   renderProducts(container, products) {
     let productListDomString = '';
-    products.forEach(product => {
-      productListDomString += `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-                  <div class="card product">
-                    <img class="card-img-top" src="img/products/${
-                      product.image
-                    }" 
-                        alt="${product.title}">
-                    <div class="card-body">
-                      <h4 class="card-title">${product.title}</h4>
-                      <p class="card-text">${product.description}</p>
-                      <button class="btn btn-info" data-toggle="modal"
-                        data-target="#productInfoModal" data-id="${
-                          product.id
-                        }">Info
-                      </button>
-                      <button class="btn btn-primary buy" data-id="${
-                        product.id
-                      }">
-                        $${product.price} - Buy
-                      </button>
-                    </div>
-                  </div>
-                </div>`;
+
+    products
+        .filter( product => product.title.toLowerCase().includes(
+                document.querySelector('.filter').value.toLowerCase()))
+        .forEach(product => {
+            productListDomString += `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <div class="card product">
+                            <img class="card-img-top" src="img/products/${
+                            product.image
+                            }"
+                                alt="${product.title}">
+                            <div class="card-body">
+                            <h4 class="card-title">${product.title}</h4>
+                            <p class="card-text">${product.description}</p>
+                            <button class="btn btn-info" data-toggle="modal"
+                                data-target="#productInfoModal" data-id="${
+                                product.id
+                                }">Info
+                            </button>
+                            <button class="btn btn-primary buy" data-id="${
+                                product.id
+                            }">
+                                $${product.price} - Buy
+                            </button>
+                            </div>
+                        </div>
+                        </div>`;
     });
     container.html(productListDomString);
   }
@@ -65,3 +72,4 @@ class ProductList {
     });
   }
 }
+
